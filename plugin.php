3b1821bb38b6b10a4563107c15887900a4f4ebfa
Arseniy Olevskiy <arsidian@gmail.com>
2023-10-24 16:11:11 +0400

    regular commit

--- plugin.php
+++ plugin.php
@@ -10,6 +10,8 @@ License: GPLv2 or later
 Text Domain: ci_cube_root_calculator
 */
 
+if (!defined('ABSPATH')) exit;
+
 if (!function_exists('add_shortcode')) return "No direct call for Cube Root Calculator by Calculator.iO";
 
 function display_ci_cube_root_calculator(){
